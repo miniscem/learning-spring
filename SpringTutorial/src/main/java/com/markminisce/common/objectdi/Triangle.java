15296e93a969179f541d50b2ca74f51292ac3ecc
Mark Minisce <miniscem@gmail.com>
2017-02-01 18:06:25 -0500

    autowire by name complete

--- SpringTutorial/src/main/java/com/markminisce/common/objectdi/Triangle.java
+++ SpringTutorial/src/main/java/com/markminisce/common/objectdi/Triangle.java
@@ -3,19 +3,51 @@ package com.markminisce.common.objectdi;
 import java.util.*; 
 
 public class Triangle {
-	private List<Point> points;
-		
-	public List<Point> getPoints() {
-		return points;
+	private Point pointA;
+	private Point pointB; 
+	private Point pointC; 
+	
+	
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
+
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
 
 	public void draw(){
-		for(Point p: this.points){
-			System.out.println(p.getX() + "," + p.getY());
-		}
+		System.out.println("Point A:" + getPointA().getX() + "," + getPointA().getY());
+		System.out.println("Point B:" + getPointB().getX() + "," + getPointB().getY());
+		System.out.println("Point C:" + getPointC().getX() + "," + getPointC().getY());
 	}
 }
