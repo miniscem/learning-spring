e3f44ee60cff43c74b4c30b91da157363264801e
Mark Minisce <miniscem@gmail.com>
2017-02-01 18:42:32 -0500

    implemented ApplicationContextAware to get information about the spring container within an individual bean

--- SpringTutorial/src/main/java/com/markminisce/common/objectdi/Triangle.java
+++ SpringTutorial/src/main/java/com/markminisce/common/objectdi/Triangle.java
@@ -1,12 +1,17 @@
 package com.markminisce.common.objectdi;
 
-import java.util.*; 
+import java.util.*;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware; 
+
+//give information from the container to the bean using the "ContextAware"
+public class Triangle implements ApplicationContextAware{
 	private Point pointA;
 	private Point pointB; 
 	private Point pointC; 
-	
+	private ApplicationContext context = null; 
 	
 
 	public Point getPointA() {
@@ -49,5 +54,17 @@ public class Triangle {
 		System.out.println("Point A:" + getPointA().getX() + "," + getPointA().getY());
 		System.out.println("Point B:" + getPointB().getX() + "," + getPointB().getY());
 		System.out.println("Point C:" + getPointC().getX() + "," + getPointC().getY());
+	}
+
+
+	//
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context = context; 
+		
+		//allows each point to be a prototype while the parent is a singleton.
+		this.setPointA((Point)context.getBean("pointA"));
+		this.setPointB((Point)context.getBean("pointB"));
+		this.setPointC((Point)context.getBean("pointC"));
+		
 	}
 }
