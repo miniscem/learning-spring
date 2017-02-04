de617592b6c53c0274b0a120fb741e60d4811109
Mark Minisce <miniscem@gmail.com>
2017-02-04 09:13:44 -0500

    Added JSR-250 annotation example

--- SpringTutorial/src/main/java/com/markminisce/annotations/AutowiredCircle.java
+++ SpringTutorial/src/main/java/com/markminisce/annotations/AutowiredCircle.java
@@ -2,6 +2,7 @@ package com.markminisce.annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import javax.annotation.*;
 
 public class AutowiredCircle implements Shape{
 
@@ -12,7 +13,7 @@ public class AutowiredCircle implements Shape{
 	}
 
 	//qualifier is something in bean definition telling spring that its a AutowiredCircle
-	//related bean
+	//related bean. @Resource allows us to specify a bean by name via JSR-250 standard annotations.
 	@Autowired
 	@Qualifier("autowiredCircleRelated")
 	public void setCenter(Point center) {
@@ -24,4 +25,13 @@ public class AutowiredCircle implements Shape{
 		System.out.println("Point is: (" + center.getX() + ", " + center.getY() + ")");
 	}
 	
+	@PostConstruct
+	public void initCircle(){
+		System.out.println("Init circle with PostContruct annotation");
+	}
+	
+	@PreDestroy
+	public void destroyCircle(){
+		System.out.println("Destroying circle with PreDestroy annotation");
+	}
 }
--- SpringTutorial/src/main/java/com/markminisce/annotations/DrawingApp.java
+++ SpringTutorial/src/main/java/com/markminisce/annotations/DrawingApp.java
@@ -1,12 +1,15 @@
 package com.markminisce.annotations;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 	public static void main(String[] args){
 		//use self-contained xml config file
-		ApplicationContext context = new ClassPathXmlApplicationContext("annotationdi.xml"); 
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("annotationdi.xml");
+		context.registerShutdownHook();
+		
 		Shape triangle = (Triangle)context.getBean("triangle"); 
 		triangle.draw();
 		
